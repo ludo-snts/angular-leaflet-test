149fadbbc0939b19ac97f89e8a55da15976f174c
ludo-snts <ludo.snts@gmail.com>
2023-09-02 20:53:54 +0200

    ajout fonctionnalité géolocalisation au clic sur bouton

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -10,6 +11,9 @@ export class MapComponent implements AfterViewInit {
 
   // private map: any;
   private map!: L.Map; // Utilisation du non-null assertion operator (https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator) si probleme avec 'private map;'
+
+  // variable pour suivre l'état de la géolocalisation
+  private isGeolocationBlocked = false;
 
   // initialisation de la carte (valeur par défaut)
   private initMap(): void {
@@ -31,7 +35,7 @@ export class MapComponent implements AfterViewInit {
 
     // ajout du zoom personnalisé
     L.control.zoom({
-      position: 'bottomright' // position du zoom personnalisé (en bas à droite)
+      position: 'bottomright' // position du zoom personnalisé (en bas à droite), le zoom par défaut est en haut à gauche et il est désactivé
   }).addTo(this.map);
 
     // TEST: ajout d'un marqueur sur la carte (position par défaut) : OK
@@ -52,12 +56,12 @@ export class MapComponent implements AfterViewInit {
 
     // TEST: ajout d'un marker sur la carte sur la position de l'utilisateur (géolocalisation) : OK
     // ajout de la localisation de l'utilisateur au chargement de la page (si l'utilisateur accepte la géolocalisation) sinon se positionne sur les coordonnées par défaut : OK
-    this.map.locate({ setView: true, maxZoom: 16 });
+    // this.map.locate({ setView: true, maxZoom: 16 });
 
-    this.map.on('locationfound', (e: L.LocationEvent) => {
-      const userMarker = L.marker(e.latlng); // Création d'un marqueur à la position de l'utilisateur
-      userMarker.addTo(this.map);
-    });
+    // this.map.on('locationfound', (e: L.LocationEvent) => {
+    //   const userMarker = L.marker(e.latlng); // Création d'un marqueur à la position de l'utilisateur
+    //   userMarker.addTo(this.map);
+    // });
 
     // TEST: ajout d'un marqueur personnalisé
     const icon = L.divIcon({
@@ -67,22 +71,51 @@ export class MapComponent implements AfterViewInit {
       // iconSize: [40, 40],
       // iconAnchor: [ 13, 41 ]
     });
+    const marker = L.marker([42.69607784382969, 2.8892290890216943], { icon }); // définir la position du marqueur
+    marker.bindPopup('<b>Hello !</b>'); // ajouter un popup au marqueur
+    marker.addTo(this.map); // ajouter le marqueur à la carte
 
-    const marker = L.marker([42.69607784382969, 2.8892290890216943], { icon });
-    marker.bindPopup('<b>Hello !</b>');
-
-    marker.addTo(this.map);
-
-    // TEST: ajout d'une searchbar
-    // var searchbox = L.Control.searchbox({
-    //   position: 'topright',
-    //   expand: 'left'
-    // });
-    // searchbox.addTo(this.map);
-
-    
 
   }
+  
+
+//fonction accompagnasnt le bouton de geolocalisation de l'utilisateur: 
+//(<button class="locate-user-btn" (click)="locateUser()">Localiser Moi</button> dans le html)
+locateUser(): void {
+  if (this.isGeolocationBlocked) {
+    // Si la géolocalisation a été bloquée précédemment, réessayez la demande
+    this.isGeolocationBlocked = false;
+  }
+
+  // Ajouter un marqueur à la position de l'utilisateur
+  this.map.on('locationfound', (e: L.LocationEvent) => {
+    // Création d'un marqueur à la position de l'utilisateur
+    const geolocIcon = L.divIcon({
+      html: "<div class='geoloc-pin'></div>",
+    });
+    const userMarker = L.marker(e.latlng, { icon: geolocIcon });
+    userMarker.addTo(this.map);
+  });
+
+  this.map.locate({ setView: true, maxZoom: 16 });
+
+  // Gérer l'événement de localisation
+  this.map.on('locationfound', (e: L.LocationEvent) => {
+    // Vous pouvez ajouter du code pour traiter la localisation ici si nécessaire
+  });
+
+  // Gérer l'événement de blocage de la géolocalisation
+  this.map.on('locationerror', (e: L.ErrorEvent) => {
+    if (e.code === 1) {
+      // Le navigateur a bloqué la géolocalisation, marquez l'état comme bloqué
+      this.isGeolocationBlocked = true;
+      // Vous pouvez afficher un message d'erreur à l'utilisateur ici si nécessaire
+      // message d'erreur par défaut
+      alert('La géolocalisation a été bloquée dans votre navigateur. Veuillez autoriser l\'accès à votre position pour utiliser cette fonctionnalité.');
+    }
+  });
+}
+
 
 
   constructor() { }
