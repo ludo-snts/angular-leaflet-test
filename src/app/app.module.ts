2b2aea7baa7654868f8af8bc8f1f907673610091
ludo-snts <ludo.snts@gmailcom>
2023-09-04 17:22:44 +0200

    custom overlay layers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,8 +12,7 @@ import { MapComponent } from './map/map.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, NgZone } from '@angular/core';
 import * as L from 'leaflet';
 
 
@@ -19,45 +19,57 @@ export class MapComponent implements AfterViewInit {
   public showSettings = false;
 
 
-
   // initialisation de la carte (valeur par défaut)
   private initMap(): void {
     this.map = L.map('map', {
       center: [42.69607784382969, 2.8892290890216943], // coordonnées GPS lattitude, longitude de Mind And Go
       zoom: 15, // zoom de la carte
-      zoomControl: false // désactivation du zoom par défaut
+      zoomControl: false, // désactivation du zoom par défaut
     });
 
     // initialisation des tuiles (valeur par défaut)
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    // TEST: initialisation des tuiles personnalisées (stockées en local dans le dossier assets/tiles/map_test)
     const customTiles = L.tileLayer('assets/tiles/map_test/{z}/{x}/{y}.png', {
       maxZoom: 15,
       minZoom: 14,
     });
 
+    // TEST: initialisation des tuiles personnalisées (stockées en local dans le dossier assets/tiles/map_test)
+    const defaultTiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+    });
+
     // ajout des tuiles à la carte
-    // customTiles.addTo(this.map);
-    tiles.addTo(this.map);
-    
-    
+    customTiles.addTo(this.map);
+    defaultTiles.addTo(this.map);
+
+    // ajout du controle des couches (tuiles)
+    const baseMaps= {
+      "Local":customTiles,
+      "En ligne":defaultTiles
+    };
+
+    // ajout du controle des couches (tuiles) bis
+    let overlayMaps = {
+      "Local":customTiles,
+      "En ligne":defaultTiles    };
+
+    L.control.layers(baseMaps, overlayMaps, {
+      position: 'bottomleft' // Spécifiez la position en bas à gauche
+    }).addTo(this.map);
+
 
     // ajout du zoom personnalisé
     L.control.zoom({
       position: 'bottomright' // position du zoom personnalisé (en bas à droite), le zoom par défaut est en haut à gauche et il est désactivé
-  }).addTo(this.map);
+    }).addTo(this.map);
 
     // TEST: ajout d'un marqueur sur la carte (position par défaut) : OK
     // const marker = L.marker([ 42.69607784382969, 2.8892290890216943 ]);
     // marker.addTo(this.map);
 
     // TEST: ajout d'un popup sur le marqueur : OK
-      // marker.bindPopup('<b>Hello !</b>');
+    // marker.bindPopup('<b>Hello !</b>');
 
     // TEST: ajout d'un cercle sur la carte (position par défaut) : OK
     // const circle = L.circle([ 42.69607784382969, 2.8892290890216943 ], {
@@ -91,60 +103,57 @@ export class MapComponent implements AfterViewInit {
 
 
   }
-  
 
-//fonction accompagnasnt le bouton de geolocalisation de l'utilisateur: 
-//(<button class="locate-user-btn" (click)="locateUser()">Localiser Moi</button> dans le html)
-locateUser(): void {
-  if (this.isGeolocationBlocked) {
-    // Si la géolocalisation a été bloquée précédemment, réessayez la demande
-    this.isGeolocationBlocked = false;
+
+  //fonction accompagnasnt le bouton de geolocalisation de l'utilisateur: 
+  //(<button class="locate-user-btn" (click)="locateUser()">Localiser Moi</button> dans le html)
+  locateUser(): void {
+    if (this.isGeolocationBlocked) {
+      // Si la géolocalisation a été bloquée précédemment, réessayez la demande
+      this.isGeolocationBlocked = false;
+    }
+
+    // Ajouter un marqueur à la position de l'utilisateur
+    this.map.on('locationfound', (e: L.LocationEvent) => {
+      // Création d'un marqueur à la position de l'utilisateur
+      const geolocIcon = L.divIcon({
+        html: "<div class='geoloc-pin'></div>",
+      });
+      const userMarker = L.marker(e.latlng, { icon: geolocIcon });
+      userMarker.addTo(this.map);
+    });
+
+    this.map.locate({ setView: true, maxZoom: 16 });
+
+    // Gérer l'événement de localisation
+    this.map.on('locationfound', (e: L.LocationEvent) => {
+      // Vous pouvez ajouter du code pour traiter la localisation ici si nécessaire
+    });
+
+    // Gérer l'événement de blocage de la géolocalisation
+    this.map.on('locationerror', (e: L.ErrorEvent) => {
+      if (e.code === 1) {
+        // Le navigateur a bloqué la géolocalisation, marquez l'état comme bloqué
+        this.isGeolocationBlocked = true;
+        // Vous pouvez afficher un message d'erreur à l'utilisateur ici si nécessaire
+        // message d'erreur par défaut
+        alert('La géolocalisation a été bloquée dans votre navigateur. Veuillez autoriser l\'accès à votre position pour utiliser cette fonctionnalité.');
+      }
+    });
+
+
+
   }
 
-  // Ajouter un marqueur à la position de l'utilisateur
-  this.map.on('locationfound', (e: L.LocationEvent) => {
-    // Création d'un marqueur à la position de l'utilisateur
-    const geolocIcon = L.divIcon({
-      html: "<div class='geoloc-pin'></div>",
-    });
-    const userMarker = L.marker(e.latlng, { icon: geolocIcon });
-    userMarker.addTo(this.map);
-  });
+  // fonction accompagnant l'apparition/disparition du menu réglages':
+  toggleSettings(): void {
+    // Inverser la valeur actuelle de showSettings
+    this.showSettings = !this.showSettings;
+  }
 
-  this.map.locate({ setView: true, maxZoom: 16 });
+  constructor() {
 
-  // Gérer l'événement de localisation
-  this.map.on('locationfound', (e: L.LocationEvent) => {
-    // Vous pouvez ajouter du code pour traiter la localisation ici si nécessaire
-  });
-
-  // Gérer l'événement de blocage de la géolocalisation
-  this.map.on('locationerror', (e: L.ErrorEvent) => {
-    if (e.code === 1) {
-      // Le navigateur a bloqué la géolocalisation, marquez l'état comme bloqué
-      this.isGeolocationBlocked = true;
-      // Vous pouvez afficher un message d'erreur à l'utilisateur ici si nécessaire
-      // message d'erreur par défaut
-      alert('La géolocalisation a été bloquée dans votre navigateur. Veuillez autoriser l\'accès à votre position pour utiliser cette fonctionnalité.');
-    }
-  });
-
-  
-
-}
-
-// fonction accompagnant l'apparition/disparition du menu réglages':
-toggleSettings(): void {
-  // Inverser la valeur actuelle de showSettings
-  this.showSettings = !this.showSettings; 
-}
-
-
-
-
-constructor() {
-
-}
+  }
 
   ngAfterViewInit(): void {
     this.initMap();
